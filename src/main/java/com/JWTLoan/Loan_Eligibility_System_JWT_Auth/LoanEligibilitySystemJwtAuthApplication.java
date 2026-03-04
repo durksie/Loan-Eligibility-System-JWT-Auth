fcc1fb2d092d75d1e79e43220a6ff9b8234371bc
Durksie <durksie1@gmail.com>
2026-03-04 09:24:16 +0200

    Project structure

---
+++ src/main/java/com/JWTLoan/Loan_Eligibility_System_JWT_Auth/LoanEligibilitySystemJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.JWTLoan.Loan_Eligibility_System_JWT_Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilitySystemJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEligibilitySystemJwtAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JWTLoan/Loan_Eligibility_System_JWT_Auth/LoanEligibilitySystemJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JWTLoan.Loan_Eligibility_System_JWT_Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEligibilitySystemJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
